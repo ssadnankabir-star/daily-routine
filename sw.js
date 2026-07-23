4088ab34cc1bad3071d657b4e2365ba0299aa676
Sadnan <ssadnankabir@gmail.com>
2026-07-23 20:32:15 +0600

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,40 @@
+const CACHE_NAME = 'daily-routine-cache-v1';
+const ASSETS = [
+  './daily_routine.html',
+  './manifest.json',
+  './icon-192.png',
+  './icon-512.png'
+];
+
+self.addEventListener('install', function(event) {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(function(cache) {
+      return cache.addAll(ASSETS).catch(function(){ /* ignore individual failures */ });
+    })
+  );
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', function(event) {
+  event.waitUntil(
+    caches.keys().then(function(keys) {
+      return Promise.all(
+        keys.filter(function(key){ return key !== CACHE_NAME; })
+            .map(function(key){ return caches.delete(key); })
+      );
+    })
+  );
+  self.clients.claim();
+});
+
+self.addEventListener('fetch', function(event) {
+  event.respondWith(
+    caches.match(event.request).then(function(cached) {
+      return cached || fetch(event.request).then(function(response) {
+        return response;
+      }).catch(function() {
+        return cached;
+      });
+    })
+  );
+});
