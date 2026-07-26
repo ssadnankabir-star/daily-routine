0665e95fc77a0cb2eac946b920392e50a6c568df
Sadnan <ssadnankabir@gmail.com>
2026-07-26 19:13:40 +0600

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'daily-routine-cache-v3';
+const CACHE_NAME = 'daily-routine-cache-v4';
 const ASSETS = [
   './',
   './index.html',
@@ -12,6 +12,8 @@ const ASSETS = [
   './js/tools.js',
   './js/prayer.js',
   './js/ai.js',
+  './js/analytics.js',
+  './js/calendar.js',
   './js/main.js'
 ];
 
